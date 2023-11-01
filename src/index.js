f3767384f4071213b22e1aa887fae073f40696a9
McLizzerson <liesbeth.fr@gmail.com>
2023-11-01 12:35:33 +0100

    add: amenity router to index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import express from "express";
+import "dotenv/config";
+import amenityRouter from "./routes/amenities.js";
 
 const app = express();
+
+// Main body of app
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
 });
 
+app.use("/amenities", amenityRouter);
+
+// Error handling below
+
+// End
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
 });
