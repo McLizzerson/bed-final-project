0104f07214caea59aa8f3a1b607881abdd60db5a
McLizzerson <liesbeth.fr@gmail.com>
2023-11-02 17:06:14 +0100

    add: query params

--- src/services/properties/getProperties.js
+++ src/services/properties/getProperties.js
@@ -1,9 +1,16 @@
 import { PrismaClient } from "@prisma/client";
 
-const getProperties = async () => {
+const getProperties = async (location, pricePerNight) => {
   const prisma = new PrismaClient();
 
-  return prisma.property.findMany();
+  const listings = await prisma.property.findMany({
+    where: {
+      location,
+      pricePerNight,
+      // amenities is in assignment but not a key within properties!!
+    },
+  });
+  return listings;
 };
 
 export default getProperties;
