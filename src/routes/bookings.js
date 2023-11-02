de7d6a9ca8a012c9f33ec565a249175bfe62570e
McLizzerson <liesbeth.fr@gmail.com>
2023-11-02 15:19:52 +0100

    create: booking router

---
+++ src/routes/bookings.js
@@ -0,0 +1,95 @@
+import express from "express";
+import getBookings from "../services/bookings/getBookings.js";
+import createBooking from "../services/bookings/createBooking.js";
+import getBookingById from "../services/bookings/getBookingById.js";
+import updateBookingById from "../services/bookings/updateBookingById.js";
+import deleteBooking from "../services/bookings/deleteBooking.js";
+
+const bookingRouter = express.Router();
+
+bookingRouter.get("/", async (req, res, next) => {
+  try {
+    const bookings = await getBookings();
+    res.status(200).json(bookings);
+  } catch (error) {
+    next(error);
+  }
+});
+
+bookingRouter.post("/", async (req, res, next) => {
+  try {
+    const {
+      userId,
+      propertyId,
+      checkinDate,
+      checkoutDate,
+      numberOfGuests,
+      totalPrice,
+      bookingStatus,
+    } = req.body;
+    const newBooking = await createBooking(
+      userId,
+      propertyId,
+      checkinDate,
+      checkoutDate,
+      numberOfGuests,
+      totalPrice,
+      bookingStatus
+    );
+    res.status(201).json(newBooking);
+  } catch (error) {
+    next(error);
+  }
+});
+
+bookingRouter.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const booking = await getBookingById(id);
+    res.status(200).json(booking);
+  } catch (error) {
+    next(error);
+  }
+});
+
+bookingRouter.put("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const {
+      userId,
+      propertyId,
+      checkinDate,
+      checkoutDate,
+      numberOfGuests,
+      totalPrice,
+      bookingStatus,
+    } = req.body;
+    const updatedBooking = await updateBookingById(
+      id,
+      userId,
+      propertyId,
+      checkinDate,
+      checkoutDate,
+      numberOfGuests,
+      totalPrice,
+      bookingStatus
+    );
+    res.status(200).json(updatedBooking);
+  } catch (error) {
+    next(error);
+  }
+});
+
+bookingRouter.delete("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const deletedBooking = await deleteBooking(id);
+    res.status(200).json({
+      message: `Booking with id: ${deletedBooking} was deleted succesfully!`,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default bookingRouter;
