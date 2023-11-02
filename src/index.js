2f45ed88fbb866a78aceb473143bf28ec6198169
McLizzerson <liesbeth.fr@gmail.com>
2023-11-02 11:06:10 +0100

    add: user router to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import "dotenv/config";
 import amenityRouter from "./routes/amenities.js";
+import userRouter from "./routes/users.js";
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.get("/", (req, res) => {
   res.send("Hello world!");
 });
 
+app.use("/users", userRouter);
 app.use("/amenities", amenityRouter);
 
 // Error handling below
