ae7aaca2087dfec8c8ef65dc941db158d3fc9277
McLizzerson <liesbeth.fr@gmail.com>
2023-11-02 11:59:20 +0100

    create: host router

---
+++ src/routes/hosts.js
@@ -0,0 +1,95 @@
+import express from "express";
+import getHosts from "../services/hosts/getHosts.js";
+import createHost from "../services/hosts/createHost.js";
+import getHostById from "../services/hosts/getHostById.js";
+import updateHostById from "../services/hosts/updateHostById.js";
+import deleteHost from "../services/hosts/deleteHost.js";
+
+const hostRouter = express.Router();
+
+hostRouter.get("/", async (req, res, next) => {
+  try {
+    const hosts = await getHosts();
+    res.status(200).json(hosts);
+  } catch (error) {
+    next(error);
+  }
+});
+
+hostRouter.post("/", async (req, res, next) => {
+  try {
+    const {
+      username,
+      password,
+      name,
+      email,
+      phoneNumber,
+      profilePicture,
+      aboutMe,
+    } = req.body;
+    const newHost = await createHost(
+      username,
+      password,
+      name,
+      email,
+      phoneNumber,
+      profilePicture,
+      aboutMe
+    );
+    res.status(201).json(newHost);
+  } catch (error) {
+    next(error);
+  }
+});
+
+hostRouter.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const host = await getHostById(id);
+    res.status(200).json(host);
+  } catch (error) {
+    next(error);
+  }
+});
+
+hostRouter.put("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const {
+      username,
+      password,
+      name,
+      email,
+      phoneNumber,
+      profilePicture,
+      aboutMe,
+    } = req.body;
+    const updatedHost = await updateHostById(
+      id,
+      username,
+      password,
+      name,
+      email,
+      phoneNumber,
+      profilePicture,
+      aboutMe
+    );
+    res.status(200).json(updatedHost);
+  } catch (error) {
+    next(error);
+  }
+});
+
+hostRouter.delete("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const deletedHost = await deleteHost(id);
+    res.status(200).json({
+      message: `Host with id: ${deletedHost} was deleted succesfully!`,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default hostRouter;
