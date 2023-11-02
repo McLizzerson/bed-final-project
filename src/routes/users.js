c36a160ecda1349dad3bf5d96cf519c3610d9a1c
McLizzerson <liesbeth.fr@gmail.com>
2023-11-02 11:05:53 +0100

    create: user router

---
+++ src/routes/users.js
@@ -0,0 +1,79 @@
+import express from "express";
+import getUsers from "../services/users/getUsers.js";
+import createUser from "../services/users/createUser.js";
+import getUserById from "../services/users/getUserById.js";
+import updateUserById from "../services/users/updateUserById.js";
+import deleteUser from "../services/users/deleteUser.js";
+
+const userRouter = express.Router();
+
+userRouter.get("/", async (req, res, next) => {
+  try {
+    const users = await getUsers();
+    res.status(200).json(users);
+  } catch (error) {
+    next(error);
+  }
+});
+
+userRouter.post("/", async (req, res, next) => {
+  try {
+    const { username, password, name, email, phoneNumber, profilePicture } =
+      req.body;
+    const newUser = await createUser(
+      username,
+      password,
+      name,
+      email,
+      phoneNumber,
+      profilePicture
+    );
+    res.status(201).json(newUser);
+  } catch (error) {
+    next(error);
+  }
+});
+
+userRouter.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const user = await getUserById(id);
+    res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
+});
+
+userRouter.put("/:id", async (req, res, next) => {
+  try {
+    const { username, password, name, email, phoneNumber, profilePicture } =
+      req.body;
+    const { id } = req.params;
+    const updatedUser = await updateUserById(
+      id,
+      username,
+      password,
+      name,
+      email,
+      phoneNumber,
+      profilePicture
+    );
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    next(error);
+  }
+});
+
+userRouter.delete("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const deletedUser = await deleteUser(id);
+    res.status(200).json({
+      message: `User with id:${deletedUser} was deleted succesfully`,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default userRouter;
