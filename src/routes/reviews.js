466dfd9a4874429b9bc07eec5a63e054e79a8cac
McLizzerson <liesbeth.fr@gmail.com>
2023-11-02 12:41:14 +0100

    create: review router

---
+++ src/routes/reviews.js
@@ -0,0 +1,68 @@
+import express from "express";
+import getReviews from "../services/reviews/getReviews.js";
+import createReview from "../services/reviews/createreview.js";
+import getReviewById from "../services/reviews/getreviewById.js";
+import updateReviewById from "../services/reviews/updatereviewById.js";
+import deleteReview from "../services/reviews/deletereview.js";
+
+const reviewRouter = express.Router();
+
+reviewRouter.get("/", async (req, res, next) => {
+  try {
+    const reviews = await getReviews();
+    res.status(200).json(reviews);
+  } catch (error) {
+    next(error);
+  }
+});
+
+reviewRouter.post("/", async (req, res, next) => {
+  try {
+    const { userId, propertyId, rating, comment } = req.body;
+    const newReview = await createReview(userId, propertyId, rating, comment);
+    res.status(201).json(newReview);
+  } catch (error) {
+    next(error);
+  }
+});
+
+reviewRouter.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const review = await getReviewById(id);
+    res.status(200).json(review);
+  } catch (error) {
+    next(error);
+  }
+});
+
+reviewRouter.put("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { userId, propertyId, rating, comment } = req.body;
+    const updatedReview = await updateReviewById(
+      id,
+      userId,
+      propertyId,
+      rating,
+      comment
+    );
+    res.status(200).json(updatedReview);
+  } catch (error) {
+    next(error);
+  }
+});
+
+reviewRouter.delete("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const deletedReview = await deleteReview(id);
+    res.status(200).json({
+      message: `review with id:${deletedReview}, has been deleted succesfully!`,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default reviewRouter;
